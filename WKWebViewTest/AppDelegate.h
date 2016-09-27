6bf592decb4a6f4e907e821263ecbd3b9aa08bf9
JacyFox <JacyFox@foxmail.com>
2016-09-27 14:31:02 +0800

    Initial Commit

---
+++ WKWebViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewTest
+//
+//  Created by Admin on 9/27/16.
+//  Copyright © 2016 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewTest
+//
+//  Created by Admin on 9/27/16.
+//  Copyright © 2016 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
